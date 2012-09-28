102240bc60e4281c811b0cd59ed5a1aee5ab66cd
Bill Caffery <billythakidd04@gmail.com>
2012-09-27 22:19:56 -0400

    updated with current layout

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Grown Kidd Creations</title>
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<header>
+			<h4>Header</h4>
+		</header>
+		<?include_once 'nav/nav.php';?>
+		<!-- <section></section>
+		<aside></aside> -->
+		<footer>Send me some love at <a href="mailto:billythakidd04@gmail.com">BillyThaKidd04@gmail.com</a></footer>
+	</body>
+</html>
---
+++ nav/nav.php
@@ -0,0 +1,69 @@
+<?//navigation bar?>
+<script>
+	var el = document.getElementsByTagName("body")[0];
+	el.className = "";
+</script>
+<noscript>
+<!--[if IE]>
+	<link rel="stylesheet" href="nav/css/ie.css">
+<![endif]-->
+</noscript>
+<link rel="stylesheet" href="nav/css/nav.css">
+	<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<nav id="topNav" class="no-js">
+	<nobr>
+		<ul>
+			<li><a href="index.php" title="Home">Home</a></li>
+			<li><a href="#" title="Projects">Projects</a>
+				<ul>
+					<li><a href="" title="Personal Projects">Personal Projects</a></li>
+					<li class="last"><a href="" title="Work Projects">Work Projects</a></li>
+				</ul>
+			</li>
+			<li><a href="" title="Contact Me">Contact Me</a>
+				<ul>
+					<li><a href="" title="FaceBook">FaceBook</a></li>
+					<li class="last"><a href="" title="Google Plus">Google +</a></li>
+				</ul>
+			</li>
+			<li><a href="#" title="About Me">About Me</a>
+				<ul>
+					<li><a href="" title="Resume">Resume</a></li>
+					<li class="last"><a href="" title="GitHub">GitHub</a></li>
+				</ul>
+			</li>
+		</ul>
+		</nobr>
+	</nav>
+	<script src="nav/js/jquery-1.8.1.js"></script>
+	<script src="nav/js/modernizr.custom.69568.js"></script>
+	<script>
+		(function($)
+		{
+			//cache nav
+			var nav = $("#topNav");
+
+			//add indicators and hovers to submenu parents
+			nav.find("li").each(function()
+			{
+				if ($(this).find("ul").length > 0)
+				{
+					$("<span>").text("").appendTo($(this).children(":first"));
+
+					//show subnav on hover
+					$(this).mouseenter(function()
+					{
+						$(this).find("ul").stop(true, true).slideDown();
+					});
+
+					//hide submenus on exit
+					$(this).mouseleave(function()
+					{
+						$(this).find("ul").stop(true, true).slideUp();
+					});
+				}
+			});
+		})(jQuery);
+	</script>
